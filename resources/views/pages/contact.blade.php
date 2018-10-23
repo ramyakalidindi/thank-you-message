4a2273a4811d68747abf86a74e85fdc88e363fd0
ramya <ramya.mounami@gmail.com>
2018-10-23 09:48:10 -0400

    Edit Submit button

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
     <div class="card mt-3 pl-2 pr-2">
-        <div class="card-title"><h1>Contact</h1>
-            <p class="lead">Please use this form to contact the site owner.</p></div>
+        <div class="card-title"><h1>Contact</h1></div>
         <div class="card-body">
             <form action="/contact" method="post">
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label for="name"> Name</label>
-                    <input name="name" type="name" class="form-control" id="name" placeholder="name@example.com">
+                    <input name="name" type="name" class="form-control" id="name" placeholder="name">
                 </div>
                 <div class="form-group">
                     <label for="email">Email address</label>
@@ -19,7 +18,7 @@
                     <label for="body">Message</label>
                     <textarea name="body" class="form-control" id="body" rows="3"></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary mb-2"> Submit</button>
+                <button type="submit" class="btn btn-primary mb-2"> Send a message</button>
             </form>
         </div>
     </div>
