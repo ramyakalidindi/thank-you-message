1923dd1d67d41a10146443cbd351855e96278763
ramya <ramya.mounami@gmail.com>
2018-10-23 22:45:15 -0400

    Updated contact file

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,27 +1,39 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="card mt-3 pl-2 pr-2">
-        <div class="card-title"><h1>Contact</h1></div>
-        <div class="card-body">
-            <form action="/contact" method="post">
-                {{ csrf_field() }}
+
+    <h1>Contact</h1>
+    <form role="form" id="contact-form" class="contact-form" method="POST" action="{{route('contact.store')}}">
+
+        {{csrf_field()}}
+
+        <div class="row">
+            <div class="col-md-6">
                 <div class="form-group">
-                    <label for="name"> Name</label>
-                    <input name="name" type="name" class="form-control" id="name" placeholder="name">
+                    <input type="text" class="form-control" name="name" autocomplete="off" id="Name" placeholder="Name">
                 </div>
+            </div>
+            <div class="col-md-6">
                 <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                    <input type="email" class="form-control" name="email" autocomplete="off" id="E-mail"
+                           placeholder="E-mail">
                 </div>
-                <div class="form-group">
-                    <label for="body">Message</label>
-                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary mb-2"> Send a message</button>
-            </form>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    <textarea class="form-control textarea" rows="3" name="Message" id="Message"
+                              placeholder="Message"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <button type="submit" class="btn main-btn pull-right">Send a message</button>
+            </div>
+        </div>
+    </form>
 
 @endsection
 
